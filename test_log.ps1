efd467dca82853d7d9fcf8ebcc1937727bfc43a9
trinnhquangha2019 <trinhquangha2019@gmail.com>
2025-12-05 14:54:53 +0700

    Clean up test files - 14:54:53

--- test_log.ps1
+++
@@ -1,5 +0,0 @@
-$logFile = "C:\AI\auto_sync_log.txt"
-$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-"[$timestamp] Test log - Script đang chạy!" | Out-File -FilePath $logFile -Encoding UTF8
-"[$timestamp] Thư mục: C:\AI" | Out-File -FilePath $logFile -Encoding UTF8 -Append
-
